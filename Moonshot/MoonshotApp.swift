17ec2f35e091e9d7fb7dee179636a7f294956d94
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-13 12:04:58 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Dmitry Sharabin on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
