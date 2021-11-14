62c53328d3f8fe7100c6d3746ee204cd48ff47f7
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-11-14 15:05:56 +0300

    Day 40 — Loading a specific kind of Codable data

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Dmitry Sharabin on 14.11.2021.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
